cd371524ebb8cb775e07ff97c1055bf3134a808f
4ndr3wch4n <chan.andrew1995@gmail.com>
2017-04-03 18:28:40 -0400

    template files laid out and simple tests working

--- startapp.js
+++ startapp.js
@@ -1,7 +1,24 @@
 var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(request, response) {
+/*http.createServer(function(request, response) {
   response.writeHead(200, {"Content-type": "text/plain"});
-  response.write("Hello World");
   response.end();
-}).listen(8080);
+})*/
+
+app.set('view engine', 'pug')
+
+app.get('/', function(req, res) { //index route
+  res.render('index')
+});
+
+app.get('/bio', function(req, res) { //index route
+  res.end("bio");
+});
+
+app.get('/about', function(req, res) { //index route
+  res.end("about");
+});
+
+app.listen(8080);
